3e317582936def82dd05a1ca2b1ee528fb689cef
Bhuwan Bhatt <bhattbhuwan13@outlook.com>
2021-12-13 21:07:46 +0545

    structuring the configs in a single folder

--- hydra/main.py
+++ hydra/main.py
@@ -1,10 +1,13 @@
 import hydra
 from omegaconf import OmegaConf, DictConfig
 
-@hydra.main(config_name="config.yaml", config_path=".")
+@hydra.main(config_name="config.yaml", config_path="./configs")
 def main(cfg: DictConfig) -> None:
     # Print the config file using `to_yaml` method which prints in a pretty manner
     print(OmegaConf.to_yaml(cfg))
+    print("="*50)
+    print(cfg.model.tokenizer, cfg.processing.batch_size, cfg.processing.max_length)
+
 
 if __name__ == "__main__":
     main()
