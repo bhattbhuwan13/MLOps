3a733c68bb4b9b5b3c98c85ca8b8b07ed0100c13
Bhuwan Bhatt <bhattbhuwan13@outlook.com>
2021-12-13 21:22:47 +0545

    added variable interoperability

--- hydra/main.py
+++ hydra/main.py
@@ -4,7 +4,7 @@ from omegaconf import OmegaConf, DictConfig
 @hydra.main(config_name="config.yaml", config_path="./configs")
 def main(cfg: DictConfig) -> None:
     # Print the config file using `to_yaml` method which prints in a pretty manner
-    print(OmegaConf.to_yaml(cfg))
+    print(OmegaConf.to_yaml(cfg, resolve=True))
     print("="*50)
     print(cfg.model.tokenizer, cfg.processing.batch_size, cfg.processing.max_length)
 
